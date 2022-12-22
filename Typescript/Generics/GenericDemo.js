045cf886c61a760ee79856a5135effdedb14a7f2
kokaneva <kokane.vina@gmail.com>
2022-12-22 14:16:06 +0530

    generics

---
+++ Typescript/Generics/GenericDemo.js
@@ -0,0 +1,17 @@
+// Array : generic
+// Set : generic
+//Map : generic
+var arr1 = [67, "hi", true, 34, "hello"];
+var arr2 = new Array(67, 5, 23, 78, 56);
+var arr3 = new Array("hi", "helo", "welcome", "bye");
+var arr4 = new Array(3);
+arr4[0] = "hi";
+arr4[1] = 67;
+arr4[2] = "hello";
+arr4.push(56);
+arr4.push("Welcome");
+console.log(arr4);
+arr4.pop();
+console.log(arr4);
+var arr5 = new Array();
+console.log(arr5.length);
