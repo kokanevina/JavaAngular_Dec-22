17f9428476abd0acee5457bd5f4c55d5c7b1d449
kokaneva <kokane.vina@gmail.com>
2022-12-23 12:35:05 +0530

    event binding, attribute binding

--- Angular/DevelopmentProject/src/app/app.component.ts
+++ Angular/DevelopmentProject/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevelopmentProject';
+  addition=0;
+  inputType="password";
+  demo(){
+    console.log(this.title);
+    
+  }
+  add(num1:string,num2:string){
+    this.addition=parseFloat(num1)+parseFloat(num2);
+  }
+  showhide(ev:any){
+    if(ev.target.checked)
+    this.inputType="text";
+    else
+    this.inputType="password";
+  }
 }
